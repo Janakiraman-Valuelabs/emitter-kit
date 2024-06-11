12a0f2c308a1f3eba6a2d06f75bd2e63a44189a3
Janakiraman-Valuelabs <113099169+Janakiraman-Valuelabs@users.noreply.github.com>
2024-06-11 10:48:06 +0530

    Convert to Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,20 @@
+import PackageDescription
+let package = Package(
+    name: "EmitterKit",
+    products: [
+        .library(
+            name: "EmitterKit",
+            targets: ["EmitterKit"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "EmitterKit",
+            dependencies: [],
+            path: "Sources/EmitterKit"),
+        .testTarget(
+            name: "EmitterKitTests",
+            dependencies: ["EmitterKit"],
+            path: "Tests/EmitterKitTests"),
+    ]
+)
